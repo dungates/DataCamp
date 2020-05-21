0d564b31670990c4ab28e247dc8bfc49239c863c
Duncan Gates <mememe645345@gmail.com>
2020-05-20 22:32:14 -0700

    Update Test.sql

--- SQL Data Science/Introduction to SQL Server/Test.sql
+++ SQL Data Science/Introduction to SQL Server/Test.sql
@@ -1,1 +1,13 @@
-#new file
+-- Select the top 20 rows from description, nerc_region and event_date
+SELECT 
+  TOP (20) description,
+  nerc_region,
+  event_date
+FROM 
+  grid 
+  -- Order by nerc_region, affected_customers & event_date
+  -- Event_date should be in descending order
+ORDER BY
+  nerc_region,
+  affected_customers,
+  event_date DESC;
